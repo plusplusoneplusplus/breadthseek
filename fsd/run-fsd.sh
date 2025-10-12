dae9e94d57ca66a83aa9c5e2d8f1b62ae2844575
Yiheng Tao <ruby092977@gmail.com>
2025-10-12 13:54:57 -0700

    delete fsd

--- fsd/run-fsd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-PYTHONPATH=.. uv run python -m fsd.cli.main "$@"
--- fsd/web/__init__.py
+++
@@ -1,5 +0,0 @@
-"""FSD Web Server module."""
-
-from web.server import app, run_server
-
-__all__ = ["app", "run_server"]
